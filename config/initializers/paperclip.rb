130979c2c555f82c6b3091495d4d08c61e8fa41e
Talal Arshad <talal7860@gmail.com>
2015-04-28 23:50:16 +0500

    paperlcip private option

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,6 @@
 #Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 #Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-#if Rails.env == "production"
+if Rails.env == "production"
   Paperclip::Attachment.default_options.merge!(
     url:                  ':s3_domain_url',
     path:                 ':class/:attachment/:id/:style/:filename',
@@ -10,7 +10,7 @@
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    s3_permissions:       :private,
+    #s3_permissions:       :private,
     s3_protocol:          'https'
   )
-#end
+end
