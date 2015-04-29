dc0d33543cb74c34a6a16901b12095579ca68356
Talal Arshad <talal7860@gmail.com>
2015-04-29 16:53:15 +0500

    toggling handled

--- app/assets/javascripts/smartrent/apartments.js
+++ app/assets/javascripts/smartrent/apartments.js
@@ -16,5 +16,58 @@ Apartment = {
     $('#q_state_eq').val(state)
     $('#q_county_eq').val(county)
     $('.apartment_listing').submit();
+  },
+  toggleCities: function(state) {
+    state = state.replace(" ", "_")
+    if ($('#citiesList' + state).css('display') == 'none')
+    {
+      $('#citiesList' + state).slideDown("slow", function(){
+        $('#citiesPlusMinus' + state).html("-");
+      });
+    }
+    else
+    {
+      $('#citiesList' + state).slideUp("slow", function(){
+        $('#citiesPlusMinus' + state).html("+");
+      });
+    }
+  },
+  toggleCounties: function(state) {
+    state = state.replace(" ", "_")
+    if ($('#countiesList' + state).css('display') == 'none')
+    {
+      $('#countiesList' + state).slideDown("slow", function(){
+        $('#countiesPlusMinus' + state).html("-");
+      });
+    }
+    else
+    {
+      $('#countiesList' + state).slideUp("slow", function(){
+        $('#countiesPlusMinus' + state).html("+");
+      });
+    }
+  },
+  showState: function(state) {
+    state = state.replace(" ", "_")
+    var id = '#results-'+state
+    if ($(id).css('display') == 'none')
+    {
+      $(id).slideDown("fast", function(){
+        $(this).removeClass('hidden')
+        $('#plusMinus' + state).html("-");
+        $('#counties' + state).slideDown("fast")
+        $('#cities' + state).slideDown("fast")
+      });
+    }
+    else
+    {
+      $(id).slideUp("fast", function(){
+        $('#counties' + state).slideUp("fast")
+        $('#cities' + state).slideUp("fast")
+        $('#plusMinus' + state).html("+");
+      });
+    }
   }
+
+
 }
