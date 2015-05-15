0291a859bb60b0030e588b2fa34cc069fbc436d5
Talal Arshad <talal7860@gmail.com>
2015-05-15 23:30:25 +0500

    residents instead of properties

--- db/migrate/20150515181634_add_home_id_to_smartrent_properties.rb
+++ db/migrate/20150515181634_add_home_id_to_smartrent_properties.rb
@@ -1,5 +1,5 @@
 class AddHomeIdToSmartrentProperties < ActiveRecord::Migration
   def change
-    add_column :smartrent_properties, :home_id, :integer
+    add_column :smartrent_residents, :home_id, :integer
   end
 end
