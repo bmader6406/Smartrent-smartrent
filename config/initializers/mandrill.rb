c197b8cbbf96d769361b89c33107439dbc4d313c
Talal Arshad <talal7860@gmail.com>
2015-07-22 01:03:19 +0500

    configuring mail to aes

--- config/initializers/mandrill.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-    :port =>           '587',
-    :address =>        'smtp.mandrillapp.com',
-    :user_name =>      MANDRILL_USERNAME,
-    :password =>       MANDRILL_APIKEY,
-    :domain =>         'heroku.com',
-    :authentication => :plain
-}
-ActionMailer::Base.delivery_method = :smtp
