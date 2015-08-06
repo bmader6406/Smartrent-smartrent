dec2891b5e09b57271c6baeb964fe3ca8e20a639
Tin Nguyen <tinnguyen@Tins-iMac.local>
2015-08-06 23:48:03 +0700

    monthly status change: allow the status to be changed from Inactive to Active

--- app/workers/smartrent/monthly_status_updater.rb
+++ app/workers/smartrent/monthly_status_updater.rb
@@ -8,59 +8,88 @@ module Smartrent
     end
   
     def self.perform(period_start = nil)
-      period_start = (period_start || Time.now.prev_month).beginning_of_month
+      period_start = (period_start || Time.now.prev_month).utc.beginning_of_month
+      
       pp "period_start: #{period_start}"
+      
       Smartrent::Resident.all.each do |r|
-        # active => inactive or active => + rewards
-        if r.smartrent_status == Smartrent::Resident.SMARTRENT_STATUS_ACTIVE
+        # it is good to catch any resident which cause the below code fail rather then stop the calculation
+        begin
           # get properties that the resident live in
-          live_in_properties = r.resident_properties.select{|p| p.move_out_date.blank? || p.move_out_date > Time.now }
+          live_in_properties = r.resident_properties.select{|p| p.move_in_date <= period_start &&  (p.move_out_date.blank? || p.move_out_date > Time.now) }
           
-          if !live_in_properties.empty?
+          # get smartrent eligible property
+          smartrent_properties = live_in_properties.select{|rp| rp.property.status.to_s.include?(Property.STATUS_CURRENT) && rp.property.is_smartrent }
+          
+          # active => inactive or active => + rewards
+          if r.smartrent_status == Smartrent::Resident.SMARTRENT_STATUS_ACTIVE
             
-            # get smartrent eligible property
-            smartrent_properties = live_in_properties.select{|rp| rp.property.status.to_s.include?(Property.STATUS_CURRENT) && rp.property.is_smartrent }
-            if smartrent_properties.present?
-               smartrent_properties.each do |rp|
+            if !live_in_properties.empty?
+              
+              if !smartrent_properties.empty?
+                create_monthly_rewards(r, smartrent_properties, period_start)
                 
-                # create monthly reward if not created for this month
-                if r.rewards.where(:property_id => rp.property_id, :type_ => Reward.TYPE_MONTHLY_AWARDS, :period_start => [period_start, period_start.end_of_month]).count == 0
-                  r.rewards.create({
-                    :property_id => rp.property_id,
-                    :type_ => Reward.TYPE_MONTHLY_AWARDS,
-                    :period_start => period_start,
-                    :period_end => period_start.end_of_month,
-                    :amount => Setting.monthly_award
-                  })
-                end
+              else #Resident doesn't live in any smartrent property, set it's expiry to 1 year from the period start
+                r.update_attributes({
+                  :smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_INACTIVE,
+                  :expiry_date => period_start + 1.year
+                })
               end
-            else #Resident doesn't live in any smartrent property, set it's expiry to 1 year from the period start
+            
+            else # resident moved out, not live in any properties
               r.update_attributes({
                 :smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_INACTIVE,
-                :expiry_date => period_start + 1.year
+                :expiry_date => period_start + 60.days
               })
-            end
-            
-          else # resident moved out, not live in any properties
-            r.update_attributes({
-              :smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_INACTIVE,
-              :expiry_date => period_start + 60.days
-            })
 
+            end
           end
-        end
       
-        # inactive => expired
-        if r.smartrent_status == Smartrent::Resident.SMARTRENT_STATUS_INACTIVE
-          
-          if period_start >= r.expiry_date
-            r.update_attributes(:smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_EXPIRED)
+          # inactive => expired or inactive => active
+          if r.smartrent_status == Smartrent::Resident.SMARTRENT_STATUS_INACTIVE
+            
+            if smartrent_properties.empty?            
+              if period_start >= r.expiry_date
+                r.update_attributes(:smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_EXPIRED)
+              end
+              
+            else
+              r.update_attributes({
+                :smartrent_status => Smartrent::Resident.SMARTRENT_STATUS_ACTIVE,
+                :expiry_date => nil
+              })
+              
+              create_monthly_rewards(r, smartrent_properties, period_start)
+            end
           end
           
+        rescue  Exception => e
+          error_details = "#{e.class}: #{e}"
+          error_details += "\n#{e.backtrace.join("\n")}" if e.backtrace
+          p "ERROR: #{error_details}"
+
+          ::Notifier.system_message("[Smartrent::MonthlyStatusUpdater] FAILURE", "ERROR DETAILS: #{error_details}",
+            ::Notifier::DEV_ADDRESS, {"from" => ::Notifier::EXIM_ADDRESS}).deliver_now
         end
-      end
+      end # /all.each
       
     end # /perform
+    
+    def self.create_monthly_rewards(resident, smartrent_properties, period_start)
+      smartrent_properties.each do |rp|
+        # create monthly reward if not created for this month
+        if resident.rewards.where(:property_id => rp.property_id, :type_ => Reward.TYPE_MONTHLY_AWARDS, :period_start => [period_start, period_start.end_of_month]).count == 0
+          resident.rewards.create({
+            :property_id => rp.property_id,
+            :type_ => Reward.TYPE_MONTHLY_AWARDS,
+            :period_start => period_start,
+            :period_end => period_start.end_of_month,
+            :amount => Setting.monthly_award,
+            :months_earned => 1
+          })
+        end
+      end
+    end
     
   end
 end
