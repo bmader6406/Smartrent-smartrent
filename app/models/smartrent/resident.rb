4b5e52b25a4f308dd95fca78caad163d444514ca
teena <teena@qburst.com>
2018-03-27 18:51:36 +0530

    skip roommate

--- app/models/smartrent/resident.rb
+++ app/models/smartrent/resident.rb
@@ -247,20 +247,22 @@ module Smartrent
 
    def get_csv
     resident = ::Resident.where(email: self.email).last 
-    return nil if resident.nil?
+    return nil if resident.nil? || resident.roommate
     resident_status = resident.unified_status.gsub('resident_','').titleize rescue nil
+    smartrent_status = 'Active' if self.smartrent_status == 'Active' || self.smartrent_status == 'Inactive'
+    smartrent_status = 'Expired' if self.smartrent_status == 'Expired'
     gender = resident.gender || 'Unknown'
     unit = resident.units.where(status: "Current").first || resident.units.where(status: "Notice").first || nil
     if unit.nil?
      return ["Nil", "Nil", "Nil", "Nil", resident.email, "Nil" ,resident.first_name, resident.last_name,
-      self.smartrent_status, resident_status, resident.gender]
+      smartrent_status, resident_status, resident.gender]
     else
      unit_is_smartrent =  unit.property.is_smartrent ? "yes" : "no"
      roommate_status = resident.roommate ? "Roommate" : "Primary Leaseholder"
      if self
       return [unit.property.name, unit.property.state.upcase, unit_is_smartrent,  unit.property.zip,
         resident.email, roommate_status, resident.first_name, resident.last_name, 
-        self.smartrent_status, resident_status, resident.gender]
+        smartrent_status, resident_status, resident.gender]
       else
         return [unit.property.name, unit.property.state.upcase, unit_is_smartrent,  unit.property.zip,
           resident.email, roommate_status, resident.first_name, resident.last_name, "NIL", 
