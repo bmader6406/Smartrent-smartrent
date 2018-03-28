c2d18317ade82526373c29185b71e9162cea3478
teena <teena@qburst.com>
2018-03-28 10:22:21 +0530

    roommate nil check

--- app/models/smartrent/resident.rb
+++ app/models/smartrent/resident.rb
@@ -247,7 +247,8 @@ module Smartrent
 
    def get_csv
     resident = ::Resident.where(email: self.email).last 
-    return nil if resident.nil? || resident.roommate
+    return nil if resident.nil? 
+    return nil if resident.roommate
     resident_status = resident.unified_status.gsub('resident_','').titleize rescue nil
     smartrent_status = 'Active' if self.smartrent_status == 'Active' || self.smartrent_status == 'Inactive'
     smartrent_status = 'Expired' if self.smartrent_status == 'Expired'
