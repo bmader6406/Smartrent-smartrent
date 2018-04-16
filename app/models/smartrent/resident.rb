22872d9c826534cec8356099de350e46ad04e542
teena <teena@qburst.com>
2018-04-16 14:53:58 +0530

    cehck

--- app/models/smartrent/resident.rb
+++ app/models/smartrent/resident.rb
@@ -247,6 +247,7 @@ module Smartrent
 
    def get_csv
     resident = ::Resident.where(email_lc: self.email).last 
+    return [self.email, "testnikil"] if resident.nil? 
     resident_status = resident.unified_status.gsub('resident_','').titleize rescue nil
     smartrent_status = 'Active' if self.smartrent_status == 'Active' || self.smartrent_status == 'Inactive'
     smartrent_status = 'Expired' if self.smartrent_status == 'Expired'
