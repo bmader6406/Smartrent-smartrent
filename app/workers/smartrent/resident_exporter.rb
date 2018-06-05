4d082e79c4212e38f3f84a3560dbdbc360ddebf6
teena <teena@qburst.com>
2018-06-05 10:14:32 +0530

    balance greater tha zero

--- app/workers/smartrent/resident_exporter.rb
+++ app/workers/smartrent/resident_exporter.rb
@@ -76,7 +76,7 @@ module Smartrent
         csv << ["Full Name", "Email", "SmartRent Balance", "SmartRent Status", "Batch"]
         
         Smartrent::Resident.includes(:rewards)
-          .where("smartrent_status IN (?) AND created_at < '#{time.end_of_quarter.to_s(:db)}'", [
+          .where("smartrent_status IN (?) AND created_at < '#{time.end_of_quarter.to_s(:db)}' AND balance > 0", [
             Smartrent::Resident::STATUS_ACTIVE, 
             Smartrent::Resident::STATUS_INACTIVE
           ]).find_in_batches do |residents|
